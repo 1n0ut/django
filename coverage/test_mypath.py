be0d0ccfed10f3b8bc4eafd0c487c23cf5f98c66
RyanDx <3608201@qq.com>
2023-05-02 14:24:06 +0800

    del

--- coverage/test_mypath.py
+++
@@ -1,9 +0,0 @@
-import is_prime
-import unittest
-
-
-class Test_is_prime(unittest.TestCase):
-
-    def test_basic(self):
-        self.assertTrue(is_prime.is_prime(3))
-        self.assertTrue(is_prime.is_prime(4))
