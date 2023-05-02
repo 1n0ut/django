7891534e5d35ce5e011d1662740407e3ef3a4625
RyanDx <3608201@qq.com>
2023-05-02 14:21:50 +0800

    add

--- site1/app1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-
-def index(request):
-    return HttpResponse("thanks! ")
-
-def user_list(request):
-    return render(request,"user_list.html")
